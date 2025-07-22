1b184ff3345358cd45273216354d963645f63502
Melissa LARBI <melissa.larbi@cea.fr>
2025-07-22 20:12:56 +0200

    iterative saud plot 1

--- src/models/iterative_learning.py
+++ src/models/iterative_learning.py
@@ -12,11 +12,67 @@ from src.models.available_models import models
 from src.config import(
     ITERATIVE_LEARNING_LOG_PATH, 
     NB_iterations_by_model, 
-    MODELS_PATHS
+    MODELS_PATHS, 
+    IDENTIFICATION_SAUD_PLOTS_PATH
 )
 
+
+
+def  plot_2(data_history, model_name): 
+    pass 
+
+def  plot_3(data_history, model_name): 
+    pass 
+
+
+def  plot_1(data_history, model_name): 
+    # plot 1: on plot juste les vrai aud, les saud identifier dans l'iteration courante, et les non-aud de base 
+    for set_name, dat in data_history.items(): 
+        data_HCCs = dat[dat["age_hepatocellular_carcinoma_dp_dr"].notna()] 
+        fig, ax = plt.subplots(figsize=(8, 6))  
+        # Tracé de la distribution des AUD
+        sns.kdeplot(
+            data=data_HCCs[data_HCCs["alcohol_use_disorders"] == 1], 
+            x="age_hepatocellular_carcinoma_dp_dr", color="orange", fill=False, 
+            common_norm=False, alpha=0.5, linewidth=2, label=f"AUD"
+        )
+        # Tracé de la distribution des non-AUD
+        sns.kdeplot(
+            data=data_HCCs[data_HCCs["alcohol_use_disorders"] == 0], 
+            x="age_hepatocellular_carcinoma_dp_dr", color="blue", fill=False, 
+            common_norm=False, alpha=0.5, linewidth=2, label=f"AUD"
+        )
+        
+        iterations = NB_iterations_by_model[model_name]
+        palette = sns.color_palette("husl", iterations)
+        
+        for i in range(iterations):
+            saud = data_HCCs[data_HCCs[f"sAUD_{i}"]==1]
+            nb_saud = len(saud) 
+            if(nb_saud!=0) :  
+                sns.kdeplot(
+                    data=saud, x="age_hepatocellular_carcinoma_dp_dr", 
+                    color=palette[i], fill=False, common_norm=False, 
+                    alpha=0.5,
+                    linewidth=2, 
+                    label=f"sAUD_{i}"
+                )
+                
+        # Configuration des axes et titre
+        ax.set_title(f"Distribution de age_hepatocellular_carcinoma_dp_dr set={set_name})", fontsize=14, pad=15)
+        ax.grid(visible=True, linestyle="--", alpha=0.5)
+                
+        # Ajouter une légende
+        ax.legend(loc="best", fontsize=10)
+        save_path = f"{IDENTIFICATION_SAUD_PLOTS_PATH[model_name]}/age_distribution_{set_name}.png"
+        plt.savefig(save_path, dpi=300, format="png")   
+       
+        
+        
+
+
+
 def do_one_iteration(logger,model_name,iteration_number,XS, YS, data): 
-    logger.info(f"******** Iterative number {iteration_number} *********")
 
     # get the best trained model : 
     best_model = train(logger, model_name, XS, YS )
@@ -63,7 +119,7 @@ def storage(logger, data_history, data_with_predictions, iteration_number, best_
     for set_name , df in data_history.items():    
         df[f"predicted_proba_{iteration_number}"] = data_with_predictions[set_name]["predicted_proba"]
         df[f"predicted_label_{iteration_number}"] = data_with_predictions[set_name]["predicted_label"]
-        df[f"sAUD_{iteration_number}"] = ((df["alcohol_use_disorders"] == 0) & (df["predicted_proba"] > best_threshold)).astype(int)
+        df[f"sAUD_{iteration_number}"] = ((data_with_predictions[set_name]["alcohol_use_disorders"] == 0) & (data_with_predictions[set_name]["predicted_proba"] > best_threshold)).astype(int)
         data_history[set_name] = df 
         
 
@@ -75,19 +131,22 @@ def iterative_train_model(model_name, logger):
     data_history = get_data(logger, DATA_PATHS["processed"]) 
     
     for i in range(NB_iterations_by_model[model_name]): 
+        logger.info(f"******** Iterative number {i} *********")
+        
         data = select_features(data) 
         XS, YS = prepare_training_data(logger, data)
         data_with_predictions, best_threshold = do_one_iteration(logger,model_name,i,XS, YS, data)
-        
         # stocker les proba des differents iteration : 
-        storage(logger, data_history, data_with_predictions, i )
+        storage(logger, data_history, data_with_predictions, i,best_threshold)
         
         # just-qu'a la dans data_with_predictions y a les prediction : donc je dois mettre les saud dans les aud pour la prochaine iteration : 
         data = update_with_saud(logger, data_with_predictions, best_threshold )
-        
-    # AHO plots 
-    # plot 1: on plot juste les vrai aud, les saud identifier dans l'iteration courante, et les non-aud de base 
     
+    # la il faut penser à stocjer data_history comme data_with prediction comme avant 
+    # AHO plots 
+    plot_1(data_history["all_data"], model_name) 
+    plot_2(data_history["all_data"], model_name)
+    plot_3(data_history["all_data"], model_name)
         
         
         
